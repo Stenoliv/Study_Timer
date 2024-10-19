8fa1925ddec65ca17d2c022ebf3c99a726d0957f
Rasmus Räihä <68077022+Ranzz02@users.noreply.github.com>
2024-10-19 19:21:37 +0300

    Profile page and stats list

--- client/src/components/study/item/StudyItem.tsx
+++ client/src/components/study/item/StudyItem.tsx
@@ -1,11 +1,25 @@
 import { Session } from "@/types/session";
 
 export interface StudyItemProps {
-  session: Session;
+	session: Session;
 }
 
 export default function StudyItem(props: StudyItemProps) {
-  const { session } = props;
+	const { session } = props;
 
-  return <div>{session.id}</div>;
+	const hours = Math.floor(session.time / 3600);
+	const minutes = Math.floor((session.time % 3600) / 60);
+	const seconds = session.time % 60;
+
+	return (
+		<div className="flex flex-col pt-2">
+			<h3 className="text-white font-semibold">{session.name}</h3>
+			<div>
+				<label className="text-white">Studied for: </label>
+				<span className="text-gray-500">
+					{hours} h {minutes} m {seconds} s
+				</span>
+			</div>
+		</div>
+	);
 }
