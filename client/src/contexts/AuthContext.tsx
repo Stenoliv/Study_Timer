c775dc15bc5ad9e90181589aa06c5986d6b6a5d4
Rasmus Räihä <68077022+Ranzz02@users.noreply.github.com>
2024-10-20 01:18:37 +0300

    fixed a few bugs

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -6,80 +6,82 @@ import { useNavigate } from "react-router-dom";
 
 // Define the shape of the authentication context
 interface AuthContextType {
-  authenticated: boolean;
-  login: (email: string, password: string) => void;
-  register: (username: string, email: string, password: string) => void;
-  logout: () => void;
+	authenticated: boolean;
+	login: (email: string, password: string) => void;
+	register: (username: string, email: string, password: string) => void;
+	logout: () => void;
 }
 
 // Create the context with an initial undefined value
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 interface AuthProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 // Create the AuthProvider component
 export const AuthProvider = (props: AuthProviderProps) => {
-  const { children } = props;
-  const { authenticated, setAuhtenticated, setUser, setTokens } =
-    useAuthStore();
-  const navigate = useNavigate();
+	const { children } = props;
+	const { authenticated, setAuhtenticated, setUser, setTokens } =
+		useAuthStore();
+	const navigate = useNavigate();
 
-  const login = (email: string, password: string) => {
-    API.post("/auth/signin", { email, password })
-      .then((response) => {
-        toast.success(`Success! Signed in as ${response.data.user.username}`);
-        setAuhtenticated(true);
-        setTokens(response.data.tokens);
-        setUser(response.data.user);
-        navigate("/");
-      })
-      .catch((error) => {
-        toast.error("Failed to Signin: " + error.message, 2500);
-      });
-  };
+	const login = (email: string, password: string) => {
+		API.post("/auth/signin", { email, password })
+			.then((response) => {
+				toast.success(`Success! Signed in as ${response.data.user.username}`);
+				setAuhtenticated(true);
+				setTokens(response.data.tokens);
+				setUser(response.data.user);
+				navigate("/");
+			})
+			.catch((error) => {
+				console.log(error);
+				toast.error("Failed to Signin: " + error.message, 2500);
+			});
+	};
 
-  const register = (username: string, email: string, password: string) => {
-    API.post("/auth/signup", {
-      username,
-      email,
-      password,
-    })
-      .then((response) => {
-        console.log(response.data);
-        toast.success(
-          `Success! Welcome to study timer ${response.data.user.username}`
-        );
-        setAuhtenticated(true);
-        setTokens(response.data.tokens);
-        setUser(response.data.user);
-        navigate("/");
-      })
-      .catch((error) => {
-        toast.error("Failed to Signup: " + error.message, 3500);
-      });
-  };
+	const register = (username: string, email: string, password: string) => {
+		API.post("/auth/signup", {
+			username,
+			email,
+			password,
+		})
+			.then((response) => {
+				console.log(response.data);
+				toast.success(
+					`Success! Welcome to study timer ${response.data.user.username}`
+				);
+				setAuhtenticated(true);
+				setTokens(response.data.tokens);
+				setUser(response.data.user);
+				navigate("/");
+			})
+			.catch((error) => {
+				console.log(error);
+				toast.error("Failed to Signup: " + error.message, 3500);
+			});
+	};
 
-  const logout = () => {
-    setAuhtenticated(false);
-    setUser(null);
-    setTokens({ access: null, refresh: null });
-    toast.success("You logged out!");
-  };
+	const logout = () => {
+		setAuhtenticated(false);
+		setUser(null);
+		setTokens({ access: null, refresh: null });
+		toast.success("You logged out!");
+	};
 
-  return (
-    <AuthContext.Provider value={{ authenticated, login, register, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider value={{ authenticated, login, register, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 // Custom hook for using auth context
 export const useAuth = (): AuthContextType => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
+	const context = useContext(AuthContext);
+	if (!context) {
+		throw new Error("useAuth must be used within an AuthProvider");
+	}
+	return context;
 };
